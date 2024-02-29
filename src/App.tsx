45c0f75170c14fb7b4d420e5bc570e8f3278932e
Gourav Saini <gourav.saini@meritech.co.in>
2024-02-29 17:59:24 +0530

    first

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './shared/Layout/Layout';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <Layout/>
+    </>
+  );
+}
+
+export default App;
